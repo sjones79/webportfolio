3cc4bd56db26df30ac7fb48eb2f6a3e761364b34
sjones79 <jonessm@umich.edu>
2016-01-25 13:14:29 -0500

    Add validation and error handling

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,9 +1,34 @@
 <?php
 
-validateForm();
+$name = $emailAddress = $message = $human = "";
+$nameErr = "Only letters and white space allowed<br>";
+$emailErr = "Invalid email format<br>";
+$humanErr = "Incorrect response to are you a human<br>";
+$errString = "";
 
-function validateForm(){
-    echo "validate form function";
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $name = cleanData($_POST['name']);
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+        $errString .= $nameError; 
+    }
+    
+    $emailAddress = cleanData($_POST['email']);
+    if (!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
+        $errString .= $emailErr;
+    }
+    $message = cleanData($_POST['message']);
+    $human = cleanData($_POST['human']);
+    if($human != '9') {
+        $errString .= $humanErr;
+    }
+}
+
+
+function cleanData($data){
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data; 
 }
 
 
